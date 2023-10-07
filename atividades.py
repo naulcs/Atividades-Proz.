1af0f15513b5c479b848ec25567436220adeed8e
naulcs <124944261+naulcs@users.noreply.github.com>
2023-10-07 13:59:42 -0300

    Update atividades.py

--- atividades.py
+++ atividades.py
@@ -11,5 +11,25 @@ while i > 0:
         print(i)
     i -= 1
 
+# Faça uma função calculadora de dois números com três parâmetros: os dois primeiros serão os números da operação e o terceiro será a entrada que definirá a operação a ser executada. Considera a seguinte definição:
+# 1. Soma
+# 2. Subtração
+# 3. Multiplicação
+# 4. Divisão
 
-    
+# Caso seja inserido um número de operação que não exista, o resultado deverá ser 0.
+
+def calculadora(num1, num2, operacao):
+    if operacao == 1:
+        return num1 + num2
+    elif operacao == 2:
+        return num1 - num2
+    elif operacao == 3:
+        return num1 * num2
+    elif operacao == 4:
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Erro: Divisão por zero"
+    else:
+        return 0
